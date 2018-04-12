92824ed75dc18297e4aecef7228b4256d4284be8
Clement Michaud <clement.michaud34@gmail.com>
2018-04-13 00:41:13 +0200

    Login to dockerhub before publishing

--- scripts/deploy_dockerhub.sh
+++ scripts/deploy_dockerhub.sh
@@ -5,5 +5,6 @@ if [ "$TRAVIS_TAG" == "" ]; then
   exit 0
 fi
 
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -t clems4ever/mesos-term:$TRAVIS_TAG .
 docker push clems4ever/mesos-term:$TRAVIS_TAG
