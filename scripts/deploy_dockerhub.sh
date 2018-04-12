89eb1ac8d532c9983f8f25b94cc697353020416d
Clement Michaud <clement.michaud34@gmail.com>
2018-04-13 00:38:57 +0200

    Build docker image with correct tag instead of retagging image

--- scripts/deploy_dockerhub.sh
+++ scripts/deploy_dockerhub.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-docker build -t clems4ever/mesos-term .
 
 if [ "$TRAVIS_TAG" == "" ]; then
   exit 0
 fi
 
-docker tag clems4ever/mesos-term clems4ever/mesos-term:$TRAVIS_TAG
+docker build -t clems4ever/mesos-term:$TRAVIS_TAG .
 docker push clems4ever/mesos-term:$TRAVIS_TAG
