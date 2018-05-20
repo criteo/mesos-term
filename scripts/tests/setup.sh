5df96ed3253eac43f5ebc09c3963e98c3f1bd7fd
Clement Michaud <clement.michaud34@gmail.com>
2018-05-20 21:44:17 +0200

    Raise delay before pushing Marathon apps

--- scripts/tests/setup.sh
+++ scripts/tests/setup.sh
@@ -6,6 +6,6 @@ docker-compose up -d
 # import users in LDAP
 ldapadd -D "cn=admin,dc=example,dc=com" -f ldap/base.ldif -w password -x
 
-sleep 8 # Wait for Marathon to start
+sleep 15 # Wait for Marathon to start
 
 ./create_apps.sh
