b354d025e45817f2845debf7b92018cbc24e470c
Clement Michaud <clement.michaud34@gmail.com>
2018-08-04 21:11:53 +0200

    Prepend MESOS_TERM to configuration env var and DEBUG_GRANTED_TO
    This will prevent conflicts with other env vars and labels.

--- src/env_vars.ts
+++ src/env_vars.ts
@@ -33,30 +33,30 @@ export interface EnvVars {
   COMMAND: string;
 }
 
-const authorizations_enabled = (process.env['LDAP_URL']) ? true : false;
+const authorizations_enabled = (process.env['MESOS_TERM_LDAP_URL']) ? true : false;
 
 function getSuperAdmins() {
-  const adminsStr = process.env['SUPER_ADMINS'];
+  const adminsStr = process.env['MESOS_TERM_SUPER_ADMINS'];
   return (adminsStr)
     ? adminsStr.split(',')
     : [];
 }
 
 export const env: EnvVars = {
-  SESSION_SECRET: getOrExit('SESSION_SECRET'),
-  JWT_SECRET: getOrExit('JWT_SECRET'),
+  SESSION_SECRET: getOrExit('MESOS_TERM_SESSION_SECRET'),
+  JWT_SECRET: getOrExit('MESOS_TERM_JWT_SECRET'),
   SUPER_ADMINS: getSuperAdmins(),
-  MESOS_MASTER_URL: getOrExit('MESOS_MASTER_URL'),
+  MESOS_MASTER_URL: getOrExit('MESOS_TERM_MESOS_MASTER_URL'),
   AUTHORIZATIONS_ENABLED: authorizations_enabled,
-  MESOS_STATE_CACHE_TIME: parseFloat(getOrExit('MESOS_STATE_CACHE_TIME')),
-  COMMAND: getOrElse('COMMAND', '/bin/sh')
+  MESOS_STATE_CACHE_TIME: parseFloat(getOrExit('MESOS_TERM_MESOS_STATE_CACHE_TIME')),
+  COMMAND: getOrElse('MESOS_TERM_COMMAND', '/bin/sh')
 };
 
 if (authorizations_enabled) {
-  env['LDAP_URL'] = getOrExit('LDAP_URL');
-  env['LDAP_BASE_DN'] = getOrExit('LDAP_BASE_DN');
-  env['LDAP_USER'] = getOrExit('LDAP_USER');
-  env['LDAP_PASSWORD'] = getOrExit('LDAP_PASSWORD');
-  env['ENABLE_PER_APP_ADMINS'] = process.env['ENABLE_PER_APP_ADMINS'] === 'true';
-  env['ENABLE_RIGHTS_DELEGATION'] = process.env['ENABLE_RIGHTS_DELEGATION'] === 'true';
+  env['LDAP_URL'] = getOrExit('MESOS_TERM_LDAP_URL');
+  env['LDAP_BASE_DN'] = getOrExit('MESOS_TERM_LDAP_BASE_DN');
+  env['LDAP_USER'] = getOrExit('MESOS_TERM_LDAP_USER');
+  env['LDAP_PASSWORD'] = getOrExit('MESOS_TERM_LDAP_PASSWORD');
+  env['ENABLE_PER_APP_ADMINS'] = process.env['MESOS_TERM_ENABLE_PER_APP_ADMINS'] === 'true';
+  env['ENABLE_RIGHTS_DELEGATION'] = process.env['MESOS_TERM_ENABLE_RIGHTS_DELEGATION'] === 'true';
 }
