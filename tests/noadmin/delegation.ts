ab29ee77630c5e1b6e646e1055a70bb088406ca1
Clement Michaud <clement.michaud34@gmail.com>
2020-01-27 21:40:46 +0100

    Fix tests.

--- tests/noadmin/delegation.ts
+++ tests/noadmin/delegation.ts
@@ -1,40 +1,20 @@
-import Assert = require('assert');
-import Bluebird = require('bluebird');
-import webdriver = require('selenium-webdriver');
 import helpers = require('../helpers');
 import Request = require('request-promise');
-import Errors = require('request-promise/errors');
 
 describe('basic routes', function () {
-    this.timeout(5000);
+    this.timeout(10000);
 
-    describe('should not access to /delegate', function () {
-        it('get', function () {
-            return helpers.withChrome(function (driver) {
-                return Bluebird.resolve(driver.get(`http://john:password@localhost:3000/delegate`))
-                    .then(function () {
-                        return Bluebird.resolve(
-                            driver.wait(webdriver.until.elementLocated(webdriver.By.css("body")), 5000))
-                    })
-                    .then(function (el) {
-                        return Bluebird.resolve(driver.wait(webdriver.until.elementTextContains(el, 'Cannot GET /delegate'), 5000));
-                    });
-            });
-        });
-
-        it('post', function () {
-            return helpers.withChrome(function (driver) {
-                return Request({ uri: 'http://john:password@localhost:3000/delegate', json: true, method: 'POST' })
-                    .then(function (res: any) {
-                        return Bluebird.reject(new Error('should not succeed'));
-                    })
-                    .catch(Errors.StatusCodeError, function (err: Errors.StatusCodeError) {
-                        if (err.statusCode == 404) {
-                            return Bluebird.resolve();
-                        }
-                        return Bluebird.reject(new Error('bad error'));
-                    });
-            });
+    it('endpoint should not be present', async function () {
+        await helpers.withChrome(async function (driver) {
+            try {
+                await Request({ uri: 'http://john:password@localhost:3000/api/delegate', json: true, method: 'POST' });
+                throw new Error("Should not be here");
+            } catch (err) {
+                if (err.statusCode == 404) {
+                    return
+                }
+                throw new Error('bad error');
+            }
         });
     });
 });
